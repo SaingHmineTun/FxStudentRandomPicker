ee6eea5f3ddf8199bf6ff5ce06756c5797658515
Sai Mao (DELL) <saisainghminetun@gmail.com>
2025-05-14 20:02:28 +0800

    May 14 Version

--- src/main/java/it/saimao/Main.java
+++ src/main/java/it/saimao/Main.java
@@ -3,6 +3,7 @@ package it.saimao;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -18,48 +19,72 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
+
+import static it.saimao.StudentData.*;
 
 public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
 
-    private static final String FONT_FAMILY = "impact";
-    private final static String[] group1 = {"Zarm Sar", "Mork Korn Kham", "Kaung Leng", "Leng Aung Kao", "Zarm Phoung Kham", "Mo Seng Inn", "Mwe Fha Hsei", "Mawn Zarm", "Lao Ngein", "Kham Hleng"};
-    private final static String[] group2 = {"Jao Ponnya", "Jao Kawliya", "Kham Lin", "Hseng Hom", "Mwe Zarm Lein", "Yown Yain"};
+    private static final String FONT_FAMILY = "Impact";
+    private List<String> listPre1, listPre2, listInter;
+
+    private Random random;
+
+    private String pickStudent(List<String> list) {
+
+        int index = random.nextInt(list.size());
+        String studentName = list.get(index);
+        list.remove(studentName);
+        return studentName;
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
         // Label : for showing student name
         Label lbName = new Label("click to pick");
-        lbName.setFont(Font.font(FONT_FAMILY, 28));
+        lbName.setTextFill(Paint.valueOf("#333333"));
+        lbName.setFont(Font.font(FONT_FAMILY, 26));
 
         // Button : for picking random student
         Button btPick = new Button("PICK");
-        btPick.setFont(Font.font(FONT_FAMILY, 18));
-
+        btPick.setFont(Font.font(FONT_FAMILY, 22));
 
         // Radio Button : for choosing between group one and two
-        RadioButton rbOne = new RadioButton("Group one");
-        rbOne.setFont(Font.font(FONT_FAMILY, 14));
-        rbOne.setSelected(true);
-        RadioButton rbTwo = new RadioButton("Group two");
-        rbTwo.setFont(Font.font(FONT_FAMILY, 14));
+        RadioButton rbInter = new RadioButton("Inter");
+        rbInter.setFont(Font.font(FONT_FAMILY, 18));
+        rbInter.setSelected(true);
+        RadioButton rbPre1 = new RadioButton("Pre 1");
+        rbPre1.setFont(Font.font(FONT_FAMILY, 18));
+        RadioButton rbPre2 = new RadioButton("Pre 2");
+        rbPre2.setFont(Font.font(FONT_FAMILY, 18));
+        // ToggleGroup : make three radio buttons only one selectable
         ToggleGroup rg = new ToggleGroup();
-        rg.getToggles().addAll(rbOne, rbTwo);
+        rg.getToggles().addAll(rbInter, rbPre1, rbPre2);
 
         // Event listeners
-        Random rd = new Random();
+        random = new Random();
         btPick.setOnAction(event -> {
+            lbName.setTextFill(Paint.valueOf("#333333"));
             String studentName;
-            if (rbOne.isSelected()) {
-                int index = rd.nextInt(group1.length);
-                studentName = group1[index];
+            if (rbInter.isSelected()) {
+                if (listInter == null || listInter.isEmpty())
+                    listInter = new LinkedList<>(Arrays.asList(studentsInter));
+                studentName = pickStudent(listInter);
+            } else if (rbPre1.isSelected()) {
+                if (listPre1 == null || listPre1.isEmpty())
+                    listPre1 = new LinkedList<>(Arrays.asList(studentsPre1));
+                studentName = pickStudent(listPre1);
             } else {
-                int index = rd.nextInt(group2.length);
-                studentName = group2[index];
+                if (listPre2 == null || listPre2.isEmpty())
+                    listPre2 = new LinkedList<>(Arrays.asList(studentsPre2));
+                studentName = pickStudent(listPre2);
             }
 
             String[] counts = {"ONE", "TWO", "THREE", ""};
@@ -71,28 +96,32 @@ public class Main extends Application {
                         e ->
                                 lbName.setText(num)));
             }
-            timeline.setOnFinished(e -> lbName.setText(studentName));
+            timeline.setOnFinished(e -> {
+                lbName.setTextFill(Paint.valueOf("#781256"));
+                lbName.setText(studentName);
+            });
             timeline.play();
-
         });
 
 
+        // HBox : for holding radio buttons
         HBox rgChoices = new HBox();
         rgChoices.setAlignment(Pos.CENTER);
-        rgChoices.setSpacing(16);
-        rgChoices.getChildren().addAll(rbOne, rbTwo);
-
+        rgChoices.setSpacing(22);
+        rgChoices.getChildren().addAll(rbInter, rbPre1, rbPre2);
 
         VBox vBox = new VBox();
+        vBox.setPadding(new Insets(30));
         vBox.setAlignment(Pos.CENTER);
-        vBox.setSpacing(24);
+        vBox.setSpacing(40);
         vBox.setBackground(Background.fill(Paint.valueOf("#8298C2")));
         vBox.getChildren().addAll(lbName, btPick, rgChoices);
 
-        Scene scene = new Scene(vBox, 300, 300);
+        Scene scene = new Scene(vBox, 400, 300);
         primaryStage.setScene(scene);
         primaryStage.getIcons().add(new Image(getClass().getResource("/lms.png").toExternalForm()));
         primaryStage.setTitle("LMS Student Picker");
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 }
---
+++ src/main/java/it/saimao/StudentData.java
@@ -0,0 +1,76 @@
+package it.saimao;
+
+public class StudentData {
+    public final static String[] studentsPre2 = {
+            "Jao Khaao Kawdami",
+            "Narng Phoung Num",
+            "Zai Han Phomn", // 2
+            "Ven. Viriya",
+            "Jao Khao Marlarwadi",
+            "Narng Hseng Aom", // 3
+            "Narng Aung Oo", // 1
+            "Narng Jaing Kham",
+            "Ven. Nyanisara",
+            "Ven. Ariya",
+            "Narng Mo Hseng",
+            "Narng Hla Han",
+            "Ven. Vicakhana",
+            "Ven. Narada",
+            "Ven. Kumuda",
+            "Ven. Dhammananda"
+    };
+
+    public final static String[] studentsPre1 = {
+            "Ven. Nyanisara",
+            "Nang Htwe Kham",
+            "Ven. Jhandopasa",
+            "Zai Aon Hleng Hsai",
+            "Jao Khao Rupanandar",
+            "Nang Hlaing Aon",
+            "Nang Khwan Ngein Hseng",
+            "Nang Hom Kham",
+            "Nang Zarm Phoung",
+            "Ven. Muninda",
+            "Ven. Gandarma",
+            "Ven. Villiya (Panglong)",
+            "Nang Phoung Kham",
+            "Ven. Sawpana",
+            "Ven. Dhammamida",
+            "Jao Khao Wimalar",
+            "Ven. Tezinda",
+            "Ven. Khantesarra",
+            "Ven. Nandiya",
+            "Ven. Sobbita",
+            "Ven. Kawvida",
+            "Nang Lao Mawn Hsai",
+            "Ven. Dhammasarmi (Hsipaw)",
+            "Ven. Nandobhasa",
+            "Ven. Sanvrar",
+            "Ven. Okkarsa",
+            "Nang Mwe Phoung",
+            "Ven. Zeindamaya",
+            "Ven. Pannyasarmi",
+            "Nang Hseng Hsai",
+            "Ven. Dhammasami (Tangyan)",
+            "Ven. Viriya (Monghsu)",
+            "Ven. Candima",
+            "Ven. Pannyavamsa",
+            "Zai Pha Hseng",
+            "Ven. Khemacara",
+            "Ven. Sarredda"
+    };
+
+    public static final String[] studentsInter = {
+            "Nang Zarm Hom Lao", // 1, 7
+            "Zai Khwan Kham Pha", // 7, 9
+            "Ven. Wonnasiri", // 5, 4
+            "Nang Mo Noung", // 9, 5
+            "Ven. Pannyasuda", // 3, 3
+            "Ven. Suvona", // 2, 1
+            "Ven. Sawbhida", // 8, 2
+            "Nang Lao Aon", // 4, 8
+            "Ven. Vilinda", // 6, 6
+            "Zai Lurn Seng"
+    };
+
+}
